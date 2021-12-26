9f88fc28242038d8aec2adbf6b3dcba964bdd32e
sefty <seftyyunitasari@gmail.com>
2021-12-26 21:53:11 +0900

    add a root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'sessions#new'
+  root 'users#index'
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
   resources :posts, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
